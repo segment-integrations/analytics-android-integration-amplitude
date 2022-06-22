698fdeda3637d8c402f729c69a2ae425984aec7e
Prayansh Srivastava <prayansh@gmail.com>
2022-06-22 14:29:19 -0700

    fix packaging

--- src/main/java/com/segment/analytics/android/integrations/amplitude/AmplitudeIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/amplitude/AmplitudeIntegration.java
@@ -1,7 +1,7 @@
 package com.segment.analytics.android.integrations.amplitude;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.amplitude.api.Amplitude;
 import com.amplitude.api.AmplitudeClient;
